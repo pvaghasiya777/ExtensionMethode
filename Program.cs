db7eb678737910ba3e4cff15cb6174e8cf58c39a
pvaghasiya777 <paragvaghasiya777@gmail.com>
2023-08-22 17:11:49 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using MyStringExtensions;
+using System;
+
+namespace MyStringExtensions
+{
+    public static class StringExtensions
+    {
+        public static string ToCurrency(this string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                throw new ArgumentException("Input string is empty or null.");
+            }
+
+            if (double.TryParse(input, out _))
+            {
+                return "$" + input;
+            }
+            else
+            {
+                throw new ArgumentException("Input string does not contain a valid number.");
+            }
+        }
+    }
+}
+
+namespace ExtensionMethodExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numericString = "123.45";
+            string nonNumericString = "Hello";
+
+            try
+            {
+                string currencyValue = numericString.ToCurrency();
+                Console.WriteLine(currencyValue);
+
+                string invalidCurrencyValue = nonNumericString.ToCurrency();
+                Console.WriteLine(invalidCurrencyValue);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+
+        }
+    }
+}
